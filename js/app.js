017f17934cf9b45d7880e2b82f637b294edb4c4c
ankit <ahuja.ankit@gmail.com>
2022-02-01 10:32:44 +0000

    Deploying to master from @ ankit/ankitahuja.com@026ea36f7de56fecdd0e9a38f2c8a5d56716a9c9 🚀

---
+++ js/app.js
@@ -0,0 +1,411 @@
+const albums = [
+  {
+    id: "tokyo",
+    title: "Tokyo",
+    photos: [
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/1.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/2.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/3.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/4.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/5.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/6.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/7.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/8.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/9.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/10.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/11.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/12.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/13.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/tokyo/tr:h-1200/14.jpg",
+      },
+    ],
+  },
+
+  {
+    id: "nyc",
+    title: "NYC",
+    photos: [
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/6.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/2.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/3.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/4.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/5.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/1.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/7.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/8.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/9.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/10.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/11.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/12.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/13.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/14.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/15.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/16.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/17.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/18.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/19.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/20.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/21.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/nyc/tr:h-1200/22.jpg",
+      },
+    ],
+  },
+
+  {
+    id: "sf",
+    title: "San Francisco",
+    photos: [
+      {
+        src: "https://ik.imagekit.io/hwked/san-francisco/tr:h-1200/6.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/san-francisco/tr:h-1200/2.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/san-francisco/tr:h-1200/3.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/san-francisco/tr:h-1200/4.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/san-francisco/tr:h-1200/5.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/san-francisco/tr:h-1200/1.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/san-francisco/tr:h-1200/7.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/san-francisco/tr:h-1200/9.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/san-francisco/tr:h-1200/10.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/san-francisco/tr:h-1200/11.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/san-francisco/tr:h-1200/12.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/san-francisco/tr:h-1200/13.jpg",
+      },
+    ],
+  },
+
+  {
+    id: "wildlife",
+    title: "Wildlife",
+    photos: [
+      {
+        src: "https://ik.imagekit.io/hwked/wildlife/tr:h-1200/1.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/wildlife/tr:h-1200/2.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/wildlife/tr:h-1200/3.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/wildlife/tr:h-1200/4.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/wildlife/tr:h-1200/5.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/wildlife/tr:h-1200/6.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/wildlife/tr:h-1200/7.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/wildlife/tr:h-1200/8.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/wildlife/tr:h-1200/9.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/wildlife/tr:h-1200/10.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/wildlife/tr:h-1200/11.jpg",
+      },
+    ],
+  },
+
+  {
+    id: "japan",
+    title: "Japan",
+    photos: [
+      {
+        src: "https://ik.imagekit.io/hwked/japan/tr:h-1200/1.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/japan/tr:h-1200/2.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/japan/tr:h-1200/3.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/japan/tr:h-1200/4.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/japan/tr:h-1200/5.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/japan/tr:h-1200/6.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/japan/tr:h-1200/7.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/japan/tr:h-1200/8.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/japan/tr:h-1200/9.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/japan/tr:h-1200/10.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/japan/tr:h-1200/11.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/japan/tr:h-1200/12.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/japan/tr:h-1200/13.jpg",
+      },
+    ],
+  },
+
+  {
+    id: "shops",
+    title: "Shops",
+    photos: [
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/10.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/2.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/3.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/4.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/5.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/6.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/7.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/8.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/9.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/10.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/11.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/12.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/13.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/14.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/15.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shops/tr:h-1200/16.jpg",
+      },
+    ],
+  },
+
+  {
+    id: "shutters",
+    title: "Shutters",
+    photos: [
+      {
+        src: "https://ik.imagekit.io/hwked/shutters/tr:h-1200/1.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shutters/tr:h-1200/2.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shutters/tr:h-1200/3.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shutters/tr:h-1200/4.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shutters/tr:h-1200/5.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shutters/tr:h-1200/6.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shutters/tr:h-1200/7.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shutters/tr:h-1200/8.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shutters/tr:h-1200/9.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shutters/tr:h-1200/10.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shutters/tr:h-1200/11.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shutters/tr:h-1200/12.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/shutters/tr:h-1200/13.jpg",
+      },
+    ],
+  },
+  {
+    id: "aerial",
+    title: "Aerial",
+    photos: [
+      {
+        src: "https://ik.imagekit.io/hwked/aerial/tr:h-1200/1.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/aerial/tr:h-1200/2.jpg",
+      },
+      {
+        src: "https://ik.imagekit.io/hwked/aerial/tr:h-1200/3.jpg",
+      },
+    ],
+  },
+];
+
+document.addEventListener("DOMContentLoaded", function () {
+  const app = Vue.createApp({
+    template: `
+      <router-view>
+        <router-view></router-view>
+      </router-view>
+    `,
+    setup: () => {
+      VuePhotos.provideStore({
+        albums,
+        photoAlbumTitleStyle: {
+          fontSize: "32px",
+          textAlign: "center",
+        },
+        photoAlbumListTitleStyle: {
+          fontSize: "24px",
+          textAlign: "center",
+        },
+      });
+    },
+  });
+
+  const PhotoGallery = app.component("photo-gallery", {
+    template: ``,
+  });
+
+  const routes = [
+    {
+      path: "/",
+      component: PhotoGallery,
+      children: VuePhotos.createRoutes(),
+    },
+  ];
+
+  const router = VueRouter.createRouter({
+    routes,
+    mode: "history",
+    history: VueRouter.createWebHashHistory(),
+  });
+
+  app.use(router).mount("#vapp");
+});
